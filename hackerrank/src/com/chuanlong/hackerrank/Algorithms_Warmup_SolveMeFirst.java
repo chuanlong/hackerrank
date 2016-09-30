562279dec8ea328b515cdccac7e043971574b152
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-09-30 09:57:32 +0800

    Algorithms Warmup SolveMeFirst

---
+++ hackerrank/src/com/chuanlong/hackerrank/Algorithms_Warmup_SolveMeFirst.java
@@ -0,0 +1,18 @@
+package com.chuanlong.hackerrank;
+
+public class Algorithms_Warmup_SolveMeFirst {
+
+	/**
+	 * https://www.hackerrank.com/challenges/solve-me-first
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+    static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below
+    	return a+b;
+     }
+	
+
+}
